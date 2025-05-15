ac52d417d523c3de14faebcf8d3b85f2d198cd2e
TheBenx03 <TheBenx03@gmail.com>
2025-05-15 15:54:27 -0400

    Formulario

---
+++ BookStore WebApp SPA/js/main.js
@@ -0,0 +1,67 @@
+// Referencias constantes (no cambian)
+
+// GENERAL
+//TODO: Revision de rubrica
+//TODO: Inicio de sesion y creacion de cuenta
+//TODO: Clase con las validaciones
+//DB: Usuario > idUsuario, Nombre, Contraseña, Rut, Correo, Autoridad
+
+// ADMIN
+//TODO: Paginas de acceso administrativo y sus formularios
+//TODO: Vista de Base de datos de libros
+//TODO: Vista de opiniones
+//TODO: CRUD de libros
+//DB: Libro > idLibro, Nombre, Autor, Lenguaje, Categorias, Nota
+//DB: Inventario > idStock, idLibro, Precio, Stock, Compras
+
+
+// USER
+//TODO: Formulario de opinion
+//TODO: Formulario de atencion al cliente
+//TODO: CRUD de opiniones
+
+//RUBRICA
+//2.1.1: Se espera que el estudiante escriba código en JavaScript que modifique elementos del
+// DOM de forma dinámica, respondiendo a eventos o acciones del usuario, como clics, envíos
+// de formulario, selección de opciones, entre otros.
+
+//2.1.2: El formulario debe incluir validaciones personalizadas con JavaScript, asegurando
+// que los datos ingresados cumplan con los requisitos definidos por el grupo, tales como
+// formato de correo electrónico, campos obligatorios, contraseñas seguras, etc.
+
+//2.1.3: El código debe integrar el uso de arreglos y objetos para almacenar y manipular datos,
+// como registros de usuarios, productos, encuestas, entre otros, dependiendo de la temática del
+// proyecto.
+
+//2.1.4: Se espera que el desarrollo esté organizado mediante funciones reutilizables y claras,
+// con nombres representativos que permitan dividir las responsabilidades del programa y
+// faciliten su mantenimiento.
+
+// Requisitos técnicos mínimos del proyecto:
+
+// Formulario funcional y validado:
+// menos un formulario debe validar datos con JavaScript puro (sin librerías externas),
+// mostrando mensajes de error personalizados al usuario.
+
+// Manipulación del DOM:
+// Deben modificarse elementos del DOM en función de las acciones del usuario, como
+// mostrar/ocultar secciones, actualizar contenido dinámicamente, agregar elementos a listas,
+// entre otros.
+
+// Uso de arreglos y objetos:
+// El sistema debe representar y manipular datos con estructuras como objetos y/o arreglos. Por
+// ejemplo, almacenar los datos ingresados en el formulario, listar elementos en pantalla o
+// generar reportes simples.
+
+// Estructura del código con funciones:
+// To.do el código debe estar correctamente estructurado usando funciones, cada una
+// cumpliendo con una tarea específica dentro de la aplicación. Se deben evitar repeticiones
+// innecesarias y promover la reutilización.
+
+//• Carpeta con archivos .html, .css y .js separados.
+//  • Documento PDF breve que describa:
+//  • Nombre y descripción de la aplicación.
+//  • Integrantes del grupo.
+//  • Problema que resuelve la aplicación.
+//  • Validaciones implementadas.
+//  • Descripción de cómo se utilizó el DOM, arreglos y objetos.
